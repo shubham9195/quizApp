35df1f7b38cc59003fd0143a995654add07a4176
shubham9195 <shubham.kr9195@gmail.com>
2020-02-22 03:25:24 +0530

    quiz-app deploy

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
       correct: []
     }
     this.handleIncreaseScore = this.handleIncreaseScore.bind(this);
-    this.handleClick= this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
 
 
   }
@@ -29,16 +29,16 @@ class App extends Component {
           let option = result.results[this.state.nr].incorrect_answers
           let random = Math.floor(Math.random() * 4)
           option.splice(random, 0, result.results[this.state.nr].correct_answer)
-          let correct= result.results[this.state.nr].correct_answer
-          console.log("check kro",correct);
-          
+          let correct = result.results[this.state.nr].correct_answer
+          console.log("check kro", correct);
+
           this.setState({
             data: result.results,
             nr: this.state.nr + 1,
             isLoaded: true,
             question: result.results[this.state.nr].question,
             options: option,
-            correct:correct
+            correct: correct
           });
         },
 
@@ -52,50 +52,48 @@ class App extends Component {
   }
   handleIncreaseScore() {
     this.setState({
-        score: this.state.score + 1
+      score: this.state.score + 1
     });
-}
-handleClick(){
-  console.log("nr check",this.state.data);
-         let option = this.state.data[this.state.nr].incorrect_answers
-          let random = Math.floor(Math.random() * 4)
-          option.splice(random, 0, this.state.data[this.state.nr].correct_answer)
-          let correct= this.state.data[this.state.nr].correct_answer
+  }
+  handleClick() {
+    console.log("nr check", this.state.data);
+    let option = this.state.data[this.state.nr].incorrect_answers
+    let random = Math.floor(Math.random() * 4)
+    option.splice(random, 0, this.state.data[this.state.nr].correct_answer)
+    let correct = this.state.data[this.state.nr].correct_answer
 
-  this.setState({
-    nr:this.state.nr+1,
-    question: this.state.data[this.state.nr].question,
-    options: option,
-    correct:correct
-})
-}
+    this.setState({
+      nr: this.state.nr + 1,
+      question: this.state.data[this.state.nr].question,
+      options: option,
+      correct: correct
+    })
+  }
   render() {
     console.log("checkdata", this.state.data);
     console.log("checkdataoption", this.state.score);
     console.log("correctans", this.state.data.correct_answer);
-
-
     const { error, isLoaded, data, nr, question } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
-    } else if(this.state.nr===this.state.data.length){
-      return(<div className="container">
-        <h1 style={{color:"#fff"}}>Quiz Completed</h1>
-        <h3 style={{color:"#fff"}}>Thank You!</h3>
-        <h4  style={{color:"#fff"}}>Score:{this.state.score}</h4>
+    } else if (this.state.nr === this.state.data.length) {
+      return (<div className="container">
+        <h1 style={{ color: "#fff" }}>Quiz Completed</h1>
+        <h3 style={{ color: "#fff" }}>Thank You!</h3>
+        <h4 style={{ color: "#fff" }}>Score:{this.state.score}</h4>
       </div>)
-    }else {
+    } else {
       return (
         <div className="container">
-            {/* <h4 style={{color:'#fff'}}>Score:{this.state.score}</h4> */}
+          {/* <h4 style={{color:'#fff'}}>Score:{this.state.score}</h4> */}
           <div className="heading">
-            <p style={{color:'#fff'}}>Round {nr}/{data.length}</p>
+            <p style={{ color: '#fff' }}>Round {nr}/{data.length}</p>
             <div className="question">
               <h3>{question}</h3>
             </div>
-            <Answers answers={this.state.options} correctAns={this.state.correct} optionClicked={this.handleClick} increaseScore={this.handleIncreaseScore}/>
+            <Answers answers={this.state.options} correctAns={this.state.correct} optionClicked={this.handleClick} increaseScore={this.handleIncreaseScore} />
           </div>
         </div>
       );
